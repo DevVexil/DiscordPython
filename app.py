5e8711dff01c9e23171ccbb34f86c9eedef01145
DevVexil <62451622+DevVexil@users.noreply.github.com>
2020-06-05 02:45:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ zug = commands.Bot(command_prefix = ".")
 async def on_ready():
     print(f"{Fore.GREEN}Bot is online{Fore.RESET}")
 
-@zug.event # Member Join
 
             #Reset Token After Video
 zug.run("NzE3OTY5NTc2NjYyNDAxMDc0.XtnicQ.c2_FhL2HPNOz4CegK30JcsF4iJk")
